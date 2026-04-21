c919207b337f4d16a0936f32030424f6df8936e5
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-04-21 14:27:16 +0200

    Ajout des Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { PartListComponent } from './components/part-list/part-list';
 import { CategorieForm } from './components/categorie-form/categorie-form';
 import { BrandForm } from './components/brand-form/brand-form';
 import { PartForm } from './components/part-form/part-form';
+import { Dashboard } from './components/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'parts', pathMatch: 'full' },
   { path: 'parts', component: PartListComponent },
   { path: 'categorie-form', component: CategorieForm },
   { path: 'brand-form', component: BrandForm },
-  { path: 'part-form', component: PartForm }
+  { path: 'part-form', component: PartForm },
+  { 
+    path: 'dashboard', 
+    component: Dashboard, 
+    canActivate: [authGuard] 
+  }
 ];
