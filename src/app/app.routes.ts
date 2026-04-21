6312b1c03a02fb4dcee90c995c9a4e01166f49aa
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-04-21 12:23:24 +0200

    Formulaires piece marque et categorie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PartListComponent } from './components/part-list/part-list';
 import { CategorieForm } from './components/categorie-form/categorie-form';
 import { BrandForm } from './components/brand-form/brand-form';
+import { PartForm } from './components/part-form/part-form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'parts', pathMatch: 'full' },
   { path: 'parts', component: PartListComponent },
   { path: 'categorie-form', component: CategorieForm },
-  { path: 'brand-form', component: BrandForm }
+  { path: 'brand-form', component: BrandForm },
+  { path: 'part-form', component: PartForm }
 ];
